11e455ed928ea5d69c4ee9e4f52f9f8d7c304529
정세영 <wjdtp93@gmail.com>
2022-04-05 21:34:04 +0900

    Add body parser

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const PORT = 3000;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
