f63f825277e14d3c82cf29d5091f065860de487b
정세영 <wjdtp93@gmail.com>
2022-04-07 14:28:47 +0900

    Add controller and router

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const productRoutes = require("./routes");
 
 const PORT = 3000;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
