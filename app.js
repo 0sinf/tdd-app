55d9669493485aa00ec7c0002cdf8319868a8b22
정세영 <wjdtp93@gmail.com>
2022-04-07 14:38:53 +0900

    Add mongoose for connect db

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const productRoutes = require("./routes/products");
 
 const PORT = 3000;
 
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongo DB connected");
+  })
+  .catch(console.log);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
