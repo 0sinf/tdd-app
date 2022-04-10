c59664ec496b18a299417cb168453a0f0b793bb5
정세영 <wjdtp93@gmail.com>
2022-04-10 22:56:21 +0900

    Add try catch for exception

--- tests/unit/products.test.js
+++ tests/unit/products.test.js
@@ -11,7 +11,7 @@ let req, res, next;
 beforeEach(() => {
   req = httpMocks.createRequest();
   res = httpMocks.createResponse();
-  next = null;
+  next = jest.fn();
 });
 
 describe("Product Controller Create", () => {
@@ -40,5 +40,13 @@ describe("Product Controller Create", () => {
     ProductModel.create.mockReturnValue(newProduct);
     await productController.createProduct(req, res, next);
     expect(res._getJSONData()).toStrictEqual(newProduct);
+  });
+
+  it("should handle errors", async () => {
+    const errorMessage = { message: "description property missing" };
+    const rejectedPromise = Promise.reject(errorMessage);
+    ProductModel.create.mockReturnValue(rejectedPromise);
+    await productController.createProduct(req, res, next);
+    expect(next).toBeCalledWith(errorMessage);
   });
 });
