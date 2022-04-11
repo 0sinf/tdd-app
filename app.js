1bd5db40ff541e5c084af6a4185a027f3a2fdd5c
정세영 <wjdtp93@gmail.com>
2022-04-11 21:32:02 +0900

    Add error handler

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
+});
+
 app.listen(PORT);
 console.log(`Running on port ${PORT}`);
 
